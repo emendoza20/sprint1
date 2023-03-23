00b160e64f2906c3e37b5611d5280c9d4573e001
elizabeth <emendoza867@gmail.com>
2023-03-23 12:10:43 +0100

    changes new

--- Entrega 1.1 Functions y Template Literals/Nivel 3/Exercise 1/ejercicio1.js
+++ Entrega 1.1 Functions y Template Literals/Nivel 3/Exercise 1/ejercicio1.js
@@ -1,16 +1,23 @@
  const array_of_functions= [
     function() {count()},
     function() {count()},
+    function() {count()},
+    function() {count()},
+    function() {count()},
     function() {count()}
 ]
 
 const numbers=[0,1,2,3,4,5,6,7,8,9]
+
 let count=()=> numbers.forEach(element=>console.log(element));
 
 /***function count(){
     numbers.forEach(element=>console.log(element));
 }***/
- 
-for (i = 0; i < array_of_functions.length; i++) {
+ /*for (i = 0; i < array_of_functions.length; i++) {
     array_of_functions[i]();
-}
+}*/
+
+array_of_functions.forEach(function(value) {
+    console.log('The current letter is: ' + value;
+  });
