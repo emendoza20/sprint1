7eda5f158586104210893e96b7f6e1ae5b3f2640
elizabeth <emendoza867@gmail.com>
2023-03-23 10:45:42 +0100

    updates

--- Entrega 1.1 Functions y Template Literals/Nivel 3/Exercise 1/ejercicio1.js
+++ Entrega 1.1 Functions y Template Literals/Nivel 3/Exercise 1/ejercicio1.js
@@ -1,25 +1,14 @@
-debugger
-const matriz= [function count(){},function count1(){},function count2(){}]
+
+const matriz= [function count(){},function count1(){}]
 
 const numbers=[0,1,2,3,4,5,6,7,8,9]
 
-
-
-
-    
 let count=()=> numbers.forEach(element=>console.log(element));
-    
 
-function count1(){
-     
-    numbers.forEach(element=>console.log(element))
+
+for(let i=0;i<=matriz.length;i++){
+
+    matriz[i];
+
+
 }
-
-function count2(){
-     
-    numbers.forEach(element=>console.log(element))
-}
-
-matriz.forEach(element)
-
- 
