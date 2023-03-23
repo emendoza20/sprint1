1d9351979156918e2a4e50f34885a1bd257ab42e
elizabeth <emendoza867@gmail.com>
2023-03-23 11:56:44 +0100

    updates2

--- Entrega 1.1 Functions y Template Literals/Nivel 3/Exercise 1/ejercicio1.js
+++ Entrega 1.1 Functions y Template Literals/Nivel 3/Exercise 1/ejercicio1.js
@@ -1,14 +1,16 @@
-
-const matriz= [function count(){},function count1(){}]
+ const array_of_functions= [
+    function() {count()},
+    function() {count()},
+    function() {count()}
+]
 
 const numbers=[0,1,2,3,4,5,6,7,8,9]
-
 let count=()=> numbers.forEach(element=>console.log(element));
 
-
-for(let i=0;i<=matriz.length;i++){
-
-    matriz[i];
-
-
+/***function count(){
+    numbers.forEach(element=>console.log(element));
+}***/
+ 
+for (i = 0; i < array_of_functions.length; i++) {
+    array_of_functions[i]();
 }
