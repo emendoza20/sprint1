ddfbf9ef93dc2e601061f0c176fbd0c1dce8e900
elizabeth <emendoza867@gmail.com>
2023-03-27 12:35:04 +0200

    changes

--- Entrega 1.2 Classes y Arrow Functions/Nivel 3/Ejercicio1/exercise1.js
+++ Entrega 1.2 Classes y Arrow Functions/Nivel 3/Ejercicio1/exercise1.js
@@ -1,10 +1,18 @@
-let Person = {
-   nom: "Elizabeth",
-    
+class Cats {
+  constructor(brand, age, sex){
+    this.brand = brand;
+    this.age =  age;
+    this.sex= sex;
+  }
+    sayHi = () => {
+    console.log(`Hello, my brand is  ${this.brand} i have ${this.age} and my sex is ${this.sex} `);
   };
-  
-Person.dirNom= () =>{console.log(Person.nom)}
-  
-  Person.dirNom(); 
+}
 
- 
+let  Bengali = new Cats("Bengalí", 3, "M");
+let  angora= new Cats("angora", 5, "F");
+ Bengali.sayHi();
+ angora.sayHi();
+//const Balines = new Cats("Balinés", 2, "F");
+
+
