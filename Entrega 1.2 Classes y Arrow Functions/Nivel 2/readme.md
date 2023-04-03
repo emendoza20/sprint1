997f2cae9a7d18fe2c7f0fd14fa15eed06918f34
Elizabeth Mendoza <emendoza867@gmail.com>
2023-04-03 19:00:58 +0200

    cambios

---
+++ Entrega 1.2 Classes y Arrow Functions/Nivel 2/readme.md
@@ -0,0 +1,5 @@
+⭐⭐ Nivell 2
+- Exercici 1
+Crea una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a valor el paràmetre rebut.
+
+Desarrollo:
--- Entrega 1.2 Classes y Arrow Functions/Nivel 3/Ejercicio1/exercise1.js
+++ Entrega 1.2 Classes y Arrow Functions/Nivel 3/Ejercicio1/exercise1.js
@@ -1,11 +1,34 @@
-const count = true;
-
-let countValue = new Promise(function (resolve, reject) {
-    if (count) {
-        resolve("There is a count value.");
-    } else {
-        reject("There is no count value");
+class Persona {
+    constructor(nom, cognom) {
+  
+      if (this.name = nom,
+        this.surname = cognom,
+        this.constructor === Persona) {
+        throw new Error("No se puede instanciar una clase abstracta")
+      };
     }
-});
-
-console.log(countValue);
+    dadesPersona() {
+      console.log(this.name + " " + this.surname);
+    }
+  }
+  
+  class Personaje extends Persona {
+    constructor(nom, cognom) {
+      super(nom, cognom);
+    }
+  }
+  
+  const f1 = (nom, cognom) => new Persona(nom, cognom);
+  const f2 = (nom, cognom) => new Personaje(nom, cognom);
+  
+  //ERROR: No se puede instanciar una clase abstracta
+  //const persona0 = f1("Johny", "Melavo")
+  
+  //Se puede instanciar una subclase
+  const persona1 = f2("Esther", "Colero");
+  const persona2 = f2("Benito", "Camela");
+  const persona3 = f2("Elver", "Galarga");
+  
+  persona1.dadesPersona()
+  persona2.dadesPersona()
+  persona3.dadesPersona()
