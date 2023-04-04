a8f6436f652e9e3e6c8f452f9160699339e8c01a
Elizabeth Mendoza <emendoza867@gmail.com>
2023-04-04 21:55:47 +0200

    changes file

--- Entrgs 1.5 Node Utils/Nivel 1/Exercise 1/exercise1.js
+++ Entrgs 1.5 Node Utils/Nivel 1/Exercise 1/exercise1.js
@@ -1,69 +1,13 @@
-let employees = [
-    {
-      id: 1,
-      name: "Linux Torvalds",
-    },
-    {
-      id: 2,
-      name: "Bill Gates",
-    },
-    {
-      id: 3,
-      name: "Jeff Bezos",
-    },
-  ];
-  
-  let salaries = [
-    {
-      id: 1,
-      salary: 4000,
-    },
-    {
-      id: 2,
-      salary: 1000,
-    },
-    {
-      id: 3,
-      salary: 2000,
-    },
-  ];
+const fs = require('fs');
 
-  const getEmployee = (id) => {
-    return new Promise((resolve, reject) => {
-       // condition
-       const finder = employees.find((elemento) => elemento.id === id);
-       if (finder) {
-         resolve(finder);
-       } else {
-         reject("ERROR No existe ese usuario");
-       }
-   
-     });
-   
-   };
-   const getSalary = (employee) => {
-    return new Promise((resolve, reject) => {
-        if (employee != undefined) {
-            const finderSalary = salaries.find((elemento) => elemento.id === employee.id);
-                if (finderSalary) {
-                    resolve(finderSalary.salary)
-                }
-            
-        }
-        reject(`Salario no encontrado`);
-    });
-  }
-  
-
-async function printEmployeeData(Id) {
-    try {
-      const employee = await getEmployee(Id);
-      const salary = await getSalary(employee);
-      console.log(`Empleado encontrado: ${employee.name}`);
-      console.log(`El salario es ${salary}`);
-    } catch (error) {
-      console.error(error);
+const Message = (mensaje) => {
+  fs.writeFile('fichero.txt', mensaje, (error) => {
+    if (error) {
+      console.log('ERROR:', error);
+    } else {
+      console.log('Se escribió con exito el mensaje en el archivo.');
     }
-  }
+  });
+}
 
-  printEmployeeData(2);
+Message('Hola este es un mensaje de prueba .');
