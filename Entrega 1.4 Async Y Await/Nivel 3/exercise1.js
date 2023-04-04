7df244255a14140d076b553f4f35b7a4af5ea0bb
Elizabeth Mendoza <emendoza867@gmail.com>
2023-04-04 21:14:03 +0200

    cambios asyn wait

--- Entrega 1.4 Async Y Await/Nivel 3/exercise1.js
+++ Entrega 1.4 Async Y Await/Nivel 3/exercise1.js
@@ -12,17 +12,29 @@ const showDouble = async(num) =>{
 example();*/
 
 
-const sumOfNumber = async (a, b, c)=> {
+const sumOfNumbers= async (a, b, c)=> {
+  if (typeof a!== 'number' || typeof b !== 'number' || typeof c !== 'number') {
+    throw new Error('Los tres parámetros deben ser números');
+  }
+
+  if (a< 0 || b < 0 || c < 0) {
+    throw new Error('Los tres parámetros deben ser números positivos');
+  }
   let ResultA,ResultB,ResultC
     ResultA = await showDouble(a);
     ResultB= await showDouble(b);
     ResultC = await showDouble(c);
   return ResultA  + ResultB + ResultC;
 }
-sumOfNumber(1, 2, 3)
+sumOfNumbers(1, '2', 'u')
   .then((result) =>
     console.log('Result sumOfNumber:', result)
   )
-  .catch((err) => console.log(err.message))
+  .catch((err) => console.log(err))
 
-
+  sumOfNumbers(1, 2, -3)
+  .then((result) =>
+    console.log('Result sumOfNumbers:', result)
+  )
+  .catch((err) => console.log(err))
+  
