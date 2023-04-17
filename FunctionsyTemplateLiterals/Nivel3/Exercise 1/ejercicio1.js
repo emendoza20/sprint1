8d36ee3a12e3da12d1c562e59bf3fe89f89c631d
Elizabeth Mendoza <emendoza867@gmail.com>
2023-04-17 19:45:54 +0200

    changes

--- FunctionsyTemplateLiterals/Nivel3/Exercise 1/ejercicio1.js
+++ FunctionsyTemplateLiterals/Nivel3/Exercise 1/ejercicio1.js
@@ -1,23 +1,11 @@
+const arrayOFunctions = [];
 
- const array_of_functions= [
-    function() {count()},
-    function() {count()},
-    function() {count()},
-    function() {count()},
-    function() {count()},
-    function() {count()},
-    function() {count()},
-    function() {count()},
-    function() {count()},
-    function() {count()}
-]
-const numbers=[0,1,2,3,4,5,6,7,8,9]
-
-let count=()=> numbers.forEach(element=>console.log(element));
-
- for (let i = 0; i < array_of_functions.length; i++) {
-        console.log(`llamada ${i}` )
-        array_of_functions[i]();
-}
-
-  
+  const functionFilled=()=>{
+    for (let i = 0; i < 10; i++) {
+      console.log(i);
+    }
+ 
+  }
+  for (let i = 0; i < 10; i++) {
+    arrayOFunctions.push(functionFilled());
+  }
